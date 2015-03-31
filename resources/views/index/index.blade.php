a5d62261f7f09da3edd8801de0ab05cba42ac732
andresbv0620 <andresbv0620@hotmail.com>
2015-03-31 11:21:52 -0500

    Ingegrar form mailchimp index y agregar deploy.php

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -73,23 +73,23 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                 <div class="da-slide">
                     <h2>El sistema móvil de recolección de datos más fácil y flexible </h2>
                     <p>Capacidades para capturar, editar y analizar información rapido y eficientemente.</p>
-                    <a href="#blog" class="da-link">Contratar Ahora</a>
+                    <a href="#blog" class="da-link">Solicitar Ahora</a>
                 </div>
                 <div class="da-slide">
                     <h2>Totalmente adaptable a sus necesidades de conteo</h2>
                     <p>Extremadamente flexible, facilmente configurable a los requerimientos de su empresa.</p>
-                    <a href="#blog" class="da-link">Contratar Ahora</a>
+                    <a href="#blog" class="da-link">Solicitar Ahora</a>
                 </div>
                 <div class="da-slide">
                     <h2>Ahorre 30% en sus procesos de recolección de datos</h2>
                     <p>Elimine los procesos basados en papel y optimice la productividad de sus empresa.</p>
-                    <a href="#blog" class="da-link">Contratar Ahora</a>
+                    <a href="#blog" class="da-link">Solicitar Ahora</a>
 
                 </div>
                 <div class="da-slide">
                     <h2>Optimice la confiabilidad en la recolección de datos en campo</h2>
                     <p>Reduzca errores en la toma y análisis de datos y ahorrar tiempo al eliminar problemas en procesos intermedios como transcripción de datos, soporte y costos de expansion.</p>
-                    <a href="#blog" class="da-link">Contratar Ahora</a>
+                    <a href="#blog" class="da-link">Solicitar Ahora</a>
 
                 </div>
             </div>
@@ -243,7 +243,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                         <li><a href="#">Registros limitados</a></li>
                     </ul>
                     <div class="cart">
-                        <a class="popup-with-zoom-anim" href="#small-dialog">Registrarse</a>
+                        <a class="popup-with-zoom-anim" href="#contact">Solicitar Ahora</a>
                     </div>
                 </div>
                 <div class="pricing-grid green">
@@ -258,7 +258,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                         <li><a href="#">Registros ilimitados</a></li>
                     </ul>
                     <div class="cart">
-                        <a class="popup-with-zoom-anim" href="#small-dialog">Comprar Ahora</a>
+                        <a class="popup-with-zoom-anim" href="#contact">Solicitar Ahora</a>
                     </div>
                 </div>
                 <div class="pricing-grid blue">
@@ -272,7 +272,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
                         <li><a href="#">Registros ilimitados</a></li>
                     </ul>
                     <div class="cart">
-                        <a class="popup-with-zoom-anim" href="#small-dialog">Comprar Ahora</a>
+                        <a class="popup-with-zoom-anim" href="#contact">Solicitar Ahora</a>
                     </div>
                 </div>
                 <div class="clear"> </div>
@@ -365,19 +365,51 @@ License URL: http://creativecommons.org/licenses/by/3.0/
         <h3>Manténgase en contacto</h3>
         <p>¿Tiene Preguntas? Siéntase libre en contactarnos, Nos encantaría escuchar de usted.</p>
         <div class="wrap">
-            <div class="con">
-                <form method="post" action="#">
-                    <input type="text"  placeholder="Nombre" required/>
-                    <input type="text"   placeholder="Email " required/>
-                    <div class="clear"> </div>
-                    <div>
-                        <textarea value="Message:" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = ' Message';}">Mensaje</textarea>
-                    </div>
-                    <div class="con-button">
-                        <input type="submit" value="Enviar" >
+            <!-- Begin MailChimp Signup Form -->
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+
+                #mc_embed_signup{
+                    background:#2D3035; clear:left; font:14px Helvetica,Arial,sans-serif;
+                    width: 500px;
+                    margin-left: auto;
+                    margin-right: auto;
+                }
+                #mc_embed_signup form{
+
+                    width: 500px;
+                }
+                label{color: #FFFFFA;}
+                /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+                   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+                <form action="//perast.us10.list-manage.com/subscribe/post?u=ea5c3a7e1ff02d62edca2891e&amp;id=c1a0606abd" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll">
+
+                        <div class="mc-field-group">
+                            <label for="mce-EMAIL">Email </label>
+                            <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-FNAME">Nombre </label>
+                            <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                        </div>
+                        <div class="mc-field-group">
+                            <label for="mce-MMERGE3">Mensaje </label>
+                            <textarea type="textarea" rows="4" cols="56" value="" name="MMERGE3" class="" id="mce-MMERGE3">Por favor especifique el plan en el cual esta interesado</textarea>
+                        </div>
+                        <div id="mce-responses" class="clear">
+                            <div class="response" id="mce-error-response" style="display:none"></div>
+                            <div class="response" id="mce-success-response" style="display:none"></div>
+                        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_ea5c3a7e1ff02d62edca2891e_c1a0606abd" tabindex="-1" value=""></div>
+                        <div class="clear"><input type="submit" value="Enviar" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                     </div>
                 </form>
             </div>
+
+            <!--End mc_embed_signup-->
             <div class="clear"> </div>
             <div class="social_icon">
                 <ul>
